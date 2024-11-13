76d64f52d6a94b7d626a258c9371a8270d74f6e5
afaq000 <afaqhussain098@gmail.com>
2024-11-13 11:54:49 +0500

    fixes

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,10 +3,10 @@ import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card'
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import Link from 'next/link';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { IoEye, IoEyeOff } from 'react-icons/io5';
 import { Formik, ErrorMessage } from 'formik';
-import * as Yup from 'yup'; 
+import * as Yup from 'yup';
 
 const validationSchema = Yup.object({
   email: Yup.string().email('Invalid email address').required('Email is required'),
@@ -24,95 +24,181 @@ const LoginPage = () => {
     console.log('Logging in with:', values);
   };
 
+  useEffect(() => {
+    // Disable scrolling on page load
+    document.body.style.overflow = 'hidden';
+
+    // Cleanup the effect when the component is unmounted
+    return () => {
+      document.body.style.overflow = 'hidden';
+    };
+  }, []);
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <CardHeader>
-        <h2 className="text-2xl font-semibold text-center">Login</h2>
-        <h2 className="text-xs text-gray-400 text-center">Get Start</h2>
-      </CardHeader>
+    <>
+      <div className="w-full relative flex items-center justify-center bg-gray-100 ">
+        <div className="flex -translate-y-1/2  items-center gap-0.5 sm2:gap-1">
+          <div className="h-7 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-6 bg-red-500 w-0.5 rounded-full"></div>
+          <div className="h-12 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-16 bg-red-500 w-0.5 rounded-full"></div>
+          <div className="h-14 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-16 bg-red-500 w-0.5 rounded-full"></div>
+          <div className="h-14 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-4 bg-yellow-500 w-0.5 rounded-full"></div>
+          <div className="h-12 bg-yellow-500 w-0.5 rounded-full"></div>
+          <div className="h-16 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-8 bg-yellow-500 w-0.5 rounded-full"></div>
+          <div className="h-9 bg-yellow-500 w-0.5 rounded-full"></div>
+          <div className="h-9 bg-red-500 w-0.5 rounded-full"></div>
+          <div className="h-5 bg-yellow-500 w-0.5 rounded-full"></div>
+          <div className="h-16 bg-red-500 w-0.5 rounded-full"></div>
+          <div className="h-7 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-6 bg-red-500 w-0.5 rounded-full"></div>
+          <div className="h-12 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-3 bg-yellow-500 w-0.5 rounded-full"></div>
+          <div className="h-6 bg-yellow-500 w-0.5 rounded-full"></div>
+          <div className="h-3 bg-red-500 w-0.5 rounded-full"></div>
+          <div className="h-12 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-16 bg-red-500 w-0.5 rounded-full"></div>
+          <div className="h-14 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-16 bg-red-500 w-0.5 rounded-full"></div>
+          <div className="h-14 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-4 bg-yellow-500 w-0.5 rounded-full"></div>
+          <div className="h-12 bg-yellow-500 w-0.5 rounded-full"></div>
+          <div className="h-16 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-8 bg-yellow-500 w-0.5 rounded-full"></div>
+          <div className="h-9 bg-yellow-500 w-0.5 rounded-full"></div>
+          <div className="h-9 bg-red-500 w-0.5 rounded-full"></div>
+          <div className="h-5 bg-yellow-500 w-0.5 rounded-full"></div>
+          <div className="h-16 bg-red-500 w-0.5 rounded-full"></div>
+          <div className="h-7 bg-red-500 w-0.5 rounded-full"></div>
+          <div className="h-16 bg-yellow-500 w-0.5 rounded-full"></div>
+          <div className="h-12 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-6 bg-yellow-500 w-0.5 rounded-full"></div>
+          <div className="h-7 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-5 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-7 bg-red-500 w-0.5 rounded-full"></div>
+          <div className="h-9 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-7 bg-red-500 w-0.5 rounded-full"></div>
+          <div className="h-6 bg-red-500 w-0.5 rounded-full"></div>
+          <div className="h-9 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-6 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-9 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-4 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-10 bg-yellow-500 w-0.5 rounded-full"></div>
+          <div className="h-16 bg-yellow-500 w-0.5 rounded-full"></div>
+          <div className="h-12 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-6 bg-yellow-500 w-0.5 rounded-full"></div>
+          <div className="h-7 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-5 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-7 bg-red-500 w-0.5 rounded-full"></div>
+          <div className="h-9 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-7 bg-red-500 w-0.5 rounded-full"></div>
+          <div className="h-6 bg-red-500 w-0.5 rounded-full"></div>
+          <div className="h-9 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-6 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-9 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-4 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-10 bg-yellow-500 w-0.5 rounded-full"></div>
+          <div className="h-16 bg-yellow-500 w-0.5 rounded-full"></div>
+          <div className="h-12 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-6 bg-yellow-500 w-0.5 rounded-full"></div>
+          <div className="h-7 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-5 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-7 bg-red-500 w-0.5 rounded-full"></div>
+          <div className="h-9 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-7 bg-red-500 w-0.5 rounded-full"></div>
+          <div className="h-6 bg-red-500 w-0.5 rounded-full"></div>
+          <div className="h-9 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-6 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-9 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-4 bg-violet-500 w-0.5 rounded-full"></div>
+          <div className="h-10 bg-yellow-500 w-0.5 rounded-full"></div>
+        </div>
+      </div>
 
-      <Formik
-        initialValues={{ email: '', password: '' }}
-        validationSchema={validationSchema}
-        onSubmit={handleLogin}
-      >
-        {({ values, handleChange, handleBlur, errors, touched, handleSubmit }) => (
-          <Card className="w-full md:w-96 shadow-lg mt-3">
-            <form onSubmit={handleSubmit}>
-              <CardContent>
-                <div className="space-y-2 pt-4">
+      <div className="w-full absolute top-7  flex flex-col items-center min-h-screen shadow-lg bg-gray-100">
+        <Card className="w-full md:w-96 shadow-lg bg-white ">
+          <CardHeader >
+            <h2 className="text-xl font-medium">Login</h2>
+            <div className='border-b pt-5'/>
+          </CardHeader>
+          
 
-                  <div>
-                    <Label htmlFor="email">Email</Label>
-                    <Input
-                      id="email"
-                      type="email"
-                      name="email"
-                      value={values.email}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      placeholder="Enter your email"
-                      className={`w-full mt-2 border ${errors.email && touched.email ? 'border-red-500' : 'border-gray-300'}`}
-                    />
-                    <ErrorMessage
-                      name="email"
-                      component="div"
-                      className="text-sm text-red-500"
-                    />
-                  </div>
-
-                  <div>
-                    <Label htmlFor="password">Password</Label>
-                    <div className="relative">
+          <Formik
+            initialValues={{ email: '', password: '' }}
+            validationSchema={validationSchema}
+            onSubmit={handleLogin}
+          >
+            {({ values, handleChange, handleBlur, errors, touched, handleSubmit }) => (
+              <form onSubmit={handleSubmit}>
+                <CardContent>
+                  <div className="space-y-6 pt-4">
+                    <div>
+                      <Label htmlFor="email" className='text-xs'>Email</Label>
                       <Input
-                        id="password"
-                        type={passwordVisible ? 'text' : 'password'}
-                        name="password"
-                        value={values.password}
+                        id="email"
+                        type="email"
+                        name="email"
+                        value={values.email}
                         onChange={handleChange}
                         onBlur={handleBlur}
-                        placeholder="Enter your password"
-                        className={`w-full mt-2 pr-10 border ${errors.password && touched.password ? 'border-red-500' : 'border-gray-300'}`}
+                        placeholder="Enter your email"
+                        className={`w-full mt-2 border ${errors.email && touched.email ? 'border-red-500' : 'border-gray-300'}`}
                       />
-                      <button
-                        type="button"
-                        onClick={handlePasswordVisibilityToggle}
-                        className="absolute inset-y-0 right-2 flex items-center bg-none text-gray-500"
-                      >
-                        {passwordVisible ? <IoEyeOff /> : <IoEye />}
-                      </button>
-                      <ErrorMessage
-                        name="password"
-                        component="div"
-                        className="text-sm text-red-500"
-                      />
+                      <ErrorMessage name="email" component="div" className="text-sm text-red-500" />
+                    </div>
+
+                    <div>
+                     <div className='w-full flex justify-between'> <Label htmlFor="password" className='text-xs'>Password</Label> </div>
+                      <div className="relative">
+                        <Input
+                          id="password"
+                          type={passwordVisible ? 'text' : 'password'}
+                          name="password"
+                          value={values.password}
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          placeholder="Enter your password"
+                          className={`w-full mt-2 pr-10 border ${errors.password && touched.password ? 'border-red-500' : 'border-gray-300'}`}
+                        />
+                        <button
+                          type="button"
+                          onClick={handlePasswordVisibilityToggle}
+                          className="absolute inset-y-0 right-2 flex items-center bg-none text-gray-500"
+                        >
+                          {passwordVisible ? <IoEyeOff /> : <IoEye />}
+                        </button>
+                        <ErrorMessage name="password" component="div" className="text-sm text-red-500" />
+                      </div>
                     </div>
                   </div>
+                </CardContent>
+
+                <CardFooter>
+                  
+                  <div className=" w-full flex justify-between items-center -mt-2">
+                  <Link href="/signup">
+                    <p className="text-gray-400 hover:underline text-xxs">Need Account ?</p>
+                  </Link>
+                  <Link href="/signup">
+                    <p className="text-gray-400 hover:underline text-xxs">Forgot Your  Password</p>
+                  </Link>
+                  
+                  <Button variant="default" type="submit" className="bg-black text-white text-xs">
+                    Login
+                  </Button>
                 </div>
-              </CardContent>
+                </CardFooter>
 
-              <CardFooter>
-                <Button variant={"default"} type="submit" className="w-full mt-4">
-                  Login
-                </Button>
-              </CardFooter>
-
-              <div className="flex justify-end -mt-2 px-4">
-                <Link href={"/signup"}>
-                  <p className="text-blue-600 hover:underline text-xxs">Forget Password</p>
-                </Link>
-              </div>
-
-              <div className="flex justify-center pb-3">
-                <Link href={"/signup"}>
-                  <p className="text-blue-600 hover:underline">Create Account</p>
-                </Link>
-              </div>
-            </form>
-          </Card>
-        )}
-      </Formik>
-    </div>
+              
+              </form>
+            )}
+          </Formik>
+        </Card>
+      </div>
+    </>
   );
 };
 
